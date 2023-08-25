17460b5c01716d92101ec12aa1ae842e9f007272
Haoran-ryan <90777296+Haoran-ryan@users.noreply.github.com>
2023-08-25 17:55:22 +1000

    logo added

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,14 +1,42 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
     <div
     className="
-      flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100"
+      flex 
+      min-h-full 
+      flex-col 
+      justify-center 
+      py-12 
+      sm:px-6 
+      lg:px-8 
+      bg-gray-100"
     >
       <div
       className="
-      sm:mx-auto sm:w-full sm:max-w-md"
+      sm:mx-auto 
+      sm:w-full 
+      sm:max-w-md"
       >
-
+        <Image 
+          alt="logo"
+          src="/images/logo.png"
+          height="48"
+          width="48"
+          className='mx-auto w-auto'
+        />
+        <h2
+        className='
+          mt-6
+          text-center
+          text-3xl
+          font-bold
+          tracking-tight
+          text-gray-900
+        '>
+          Sign in your account
+        </h2>
       </div>
     </div>
   )
